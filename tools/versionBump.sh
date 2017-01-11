2b4cde47d8d178d88f1542826b6074cacb749f9b
Samuel Bailey <sam@bailey.geek.nz>
2017-01-12 11:47:59 +1300

    bitbucket pipelines config changes

--- tools/versionBump.sh
+++ tools/versionBump.sh
@@ -2,4 +2,4 @@ python3 versionBump.py
 git config --global user.email "sam@bailey.geek.nz"
 git config --global user.name "Bitbucket Pipelines"
 git remote set-url https://$USERNAME:$PASSWORD@bitbucket.org/bailus/kodi-addons.git
-git commit -am "Version bump (using bitbucket pipelines)" && git push HEAD:master
+git commit -am "Version bump (using bitbucket pipelines)" && git push HEAD:master HEAD:master
