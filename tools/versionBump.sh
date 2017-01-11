57efa7251519bab138ba3f905e595b0d2fa329db
Samuel Bailey <sam@bailey.geek.nz>
2017-01-12 12:37:25 +1300

    bitbucket pipelines config changes

--- tools/versionBump.sh
+++ tools/versionBump.sh
@@ -1,6 +1,6 @@
 python3 versionBump.py
 git config --global user.email "sam@bailey.geek.nz"
 git config --global user.name "Bitbucket Pipelines"
-git remote set-url origin https://$USERNAME:$PASSWORD@bitbucket.org/bailus/kodi-addons.git
+git remote set-url origin https://$USERNAME:$PASSWORD@bitbucket.org/bailus/hax-for-kodi.git
 git fetch
 git commit -am "Version bump (using bitbucket pipelines)" && git push
