bf9879d0919104ee1be7fadb9fc37faa6e5a716e
Samuel Bailey <sam@bailey.geek.nz>
2017-01-12 12:53:01 +1300

    bitbucket pipelines config changes

--- tools/versionBump.sh
+++ tools/versionBump.sh
@@ -3,4 +3,5 @@ git config --global user.email "sam@bailey.geek.nz"
 git config --global user.name "Bitbucket Pipelines"
 git remote set-url origin https://$USERNAME:$PASSWORD@bitbucket.org/bailus/hax-for-kodi.git
 git fetch
-git commit -am "Version bump (using bitbucket pipelines)" && git push
+git commit -am "Version bump (using bitbucket pipelines)"
+git push origin HEAD:master
